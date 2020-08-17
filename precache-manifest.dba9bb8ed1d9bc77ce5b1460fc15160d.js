d2829a4a3dd2b8fe26bd6cacb8031e147a5ef1e2
xtracynic <xtracynic@users.noreply.github.com>
2020-08-18 00:05:17 +0200

    Updates

---
+++ precache-manifest.dba9bb8ed1d9bc77ce5b1460fc15160d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c385e403054de978028036182402f24",
+    "url": "/yt-application/index.html"
+  },
+  {
+    "revision": "bfec2779a2093ca3487a",
+    "url": "/yt-application/static/css/main.4965ec2a.chunk.css"
+  },
+  {
+    "revision": "35e5d30d56b02a9f4bad",
+    "url": "/yt-application/static/js/2.2605bfb9.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/yt-application/static/js/2.2605bfb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfec2779a2093ca3487a",
+    "url": "/yt-application/static/js/main.e11741e6.chunk.js"
+  },
+  {
+    "revision": "db07b71f470527cca3c3",
+    "url": "/yt-application/static/js/runtime-main.e8781857.js"
+  }
+]);
